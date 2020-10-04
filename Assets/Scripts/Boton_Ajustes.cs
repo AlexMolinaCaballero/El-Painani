632c13db9951d34b3446b859dd31a429cc41414a
AlexMolinaCaballero <amolina16@alumnos.utalca.cl>
2020-10-04 15:37:17 -0300

    Inicio proyecto
    Codigo fuente del juego

---
+++ Assets/Scripts/Boton_Ajustes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton_Ajustes : MonoBehaviour
+{
+    public GameObject panelAjustes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelAjustes.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivarAjustes()
+    {
+        if (panelAjustes.activeSelf == true)
+        {
+            DesactivarAjustes();
+        }
+        else
+        {
+            panelAjustes.SetActive(true);
+        }
+    }
+
+    public void DesactivarAjustes()
+    {
+        panelAjustes.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Boton_Creditos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton_Creditos : MonoBehaviour
+{
+    public GameObject panelCreditos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelCreditos.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivarCreditos()
+    {
+        if (panelCreditos.activeSelf == true)
+        {
+            DesactivarCreditos();
+        }
+        else
+        {
+            panelCreditos.SetActive(true);
+        }
+    }
+
+    public void DesactivarCreditos()
+    {
+        panelCreditos.SetActive(false);
+    }
+}
